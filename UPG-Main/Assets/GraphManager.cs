4d2c11f693ad36ae2ff4230da36736e7abdda8a0
E-Meister2012 <ekereus@hotmail.com>
2024-03-04 10:27:26 +0100

    Added stuff
    Added graph
    Added Arrow
    Added Player Movement + Dice
    Added graph from txt file

---
+++ UPG-Main/Assets/GraphManager.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GraphManager : MonoBehaviour
+{
+    public Graph board { get; private set; }
+    int count = 0;
+    Edge edge;
+    void Start()
+    {
+        board = new Graph();
+        MakeNodes();
+        MakeEdges();
+    }
+    void MakeNodes()
+    {
+        string s;
+        List<float> split = new List<float>();
+        StreamReader reader = new StreamReader(Application.dataPath + "/textfiles/nodes.txt");
+        while ((s = reader.ReadLine()) != null)
+        {
+            split.Clear();
+            foreach(string str in s.Split(' '))
+                split.Add(float.Parse(str));
+            Vector3 vector = new Vector3(split[0], split[1], split[2]);
+            Color color = new Color(split[3], split[4], split[5]);
+            board.nodes.Add(new Node() { position = vector, nodeColor = color, radius = 0.125f });
+        }
+    }
+    void MakeEdges()
+    {
+        string s;
+        List<int> splitint = new List<int>();
+        List<float> splitcolor = new List<float>();
+        StreamReader reader = new StreamReader(Application.dataPath + "/textfiles/edges.txt");
+        while ((s = reader.ReadLine()) != null)
+        {
+            splitcolor = ReturnSplit(s);
+            s = reader.ReadLine();
+            splitint = ReturnSplit(s).Select(f => (int)Math.Ceiling(f)).ToList();
+            Color color = new Color(splitcolor[0], splitcolor[1], splitcolor[2]);
+            List<Node> toNodes = new List<Node>();
+            for (int i = 1; i < splitint.Count; i++)
+                toNodes.Add(board.nodes[splitint[i]]);  
+            edge = new Edge() {  toNodes = toNodes, edgeColor = color};
+            board.nodes[splitint[0]].edge = edge;
+        }
+    }
+    List<float> ReturnSplit(string s)
+    {
+        List<float> split = new List<float>();
+        foreach (string str in s.Split(" "))
+            split.Add(float.Parse(str)); 
+        return split;
+    }
+
+    void OnDrawGizmos()
+    {
+        if(board == null) 
+            Start();
+        foreach (var node in board.nodes)
+        {
+            Gizmos.color = node.nodeColor;
+            Gizmos.DrawSphere(node.position, node.radius);
+            if (node.edge != null)
+            {
+                foreach (var toNode in node.edge.toNodes)
+                {
+                    Gizmos.color = node.edge.edgeColor;
+                    Gizmos.DrawLine(node.position, toNode.position);
+                }
+            }
+        }
+        if (count == 0)
+            count++;
+    }
+
+}
---
+++ UPG-Main/Assets/PlayerMovement.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Node currentNode;
+    public GameObject Arrow;
+    List<GameObject> arrows = new List<GameObject>();
+    public float playerSpeed = 5;
+    int diceRoll = 0;
+    System.Random rand = new System.Random();
+    public TextMeshPro roll;
+    public float rollMargin = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("GetStartPos", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            StartCoroutine(RollDice());
+        if (diceRoll > 0)
+            MovePlayer();
+    }
+    void GetStartPos() 
+    {
+        GraphManager manager = GameObject.FindGameObjectWithTag("Tiles").GetComponent<GraphManager>();
+        currentNode = manager.board.nodes[0];
+        transform.position = currentNode.position + Vector3.up * (currentNode.radius + transform.localScale.y / 2);
+        roll.transform.position = transform.position + Vector3.up * rollMargin;
+        roll.enabled = false;
+        
+        SpawnArrows();
+    }
+    void MovePlayer()
+    {
+        Node nextNode = currentNode.edge.toNodes[0];
+        Vector3 target = nextNode.position + Vector3.up * (currentNode.radius + transform.localScale.y / 2);
+        transform.position = Vector3.MoveTowards(transform.position, target, playerSpeed);
+        if(transform.position == target)
+        {
+            currentNode = nextNode;
+            diceRoll--;
+            roll.enabled = diceRoll > 0;
+            roll.text = diceRoll.ToString();
+        }
+        SpawnArrows();
+    }
+    void SpawnArrows()
+    {
+        foreach (GameObject arrow in arrows)
+            Destroy(arrow);
+        if (currentNode.edge.toNodes.Count > 1)
+        {
+            foreach (Node toNode in currentNode.edge.toNodes)
+            {
+                GameObject arrow = Instantiate(Arrow, Vector3.zero, transform.rotation);
+                ArrowPointer pointer = arrow.GetComponent<ArrowPointer>();
+                pointer.to = toNode.position;
+                arrows.Add(arrow);
+            }
+        }
+    }
+    IEnumerator RollDice()
+    {
+        roll.enabled = true;
+        float timer = 0;
+        float rollTime = 1f; 
+
+        while (timer < rollTime)
+        {
+            int tempDice = rand.Next(1, 7);
+            roll.text = tempDice.ToString();
+            yield return new WaitForSeconds(0.05f);
+            timer += 0.05f;
+        }
+        diceRoll = rand.Next(1, 7);
+        roll.text = diceRoll.ToString();
+    }
+
+}
